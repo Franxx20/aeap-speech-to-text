a622c053c38b0d6de3340defdcfab31f57cce438
Fran <fgarcete@alumno.unlam.edu.ar>
2024-11-05 12:27:47 -0300

    working connection first implementation

--- index.js
+++ index.js
@@ -1,108 +1,110 @@
 const {Codecs} = require("./lib/codecs");
 const {Languages} = require("./lib/languages");
 const {getServer} = require("./lib/server");
-const {dispatch, handle_whisper_message} = require("./lib/dispatcher");
+const {dispatch} = require("./lib/dispatcher");
 const {WebSocket} = require('ws');
 const {randomUUID} = require("crypto");
 const crypto = require('crypto');
 const argv = require("yargs/yargs")(process.argv.slice(2))
     .command("$0 [options]", "Start a speech to text server", {
         port: {
-            alias: "p",
-            desc: "Port to listen on",
-            default: 9099,
-            type: "number",
-            group: "Server",
+            alias: "p", desc: "Port to listen on", default: 9099, type: "number", group: "Server",
         },
     })
-    .strict()
-    .argv;
+    .strict().argv;
 
 const codecs = new Codecs(argv);
 const languages = new Languages(argv);
 const server = getServer("ws", argv);
-console.log(codecs)
 
 // Initialize WebSocket connection with Whisper server
 const whisperWebSocket = new WebSocket('ws://127.0.0.1:9090');
-// console.log(whisperWebSocket)
-
-
-const clients = new Set();
+const clients = new Set(); // Track client connections
 
 whisperWebSocket.on('open', () => {
-    const data = {
-        uid: crypto.randomUUID(),
-        language: 'en_US',
-        task: 'transcribe',
-        model: 'tiny.en',
-        use_vad: true,
+    const initialData = {
+        uid: crypto.randomUUID(), language: 'en_US', task: 'transcribe', model: 'tiny.en', use_vad: true,
     };
     console.log('Opened connection with Whisper');
-    whisperWebSocket.send(JSON.stringify(data));
+    whisperWebSocket.send(JSON.stringify(initialData));
 });
 
-// whisperWebSocket.on('message', (transcription) => {
-//     // message = transcription.toString()
-//     json_message = JSON.parse(transcription)
-//     console.log('Received transcription:', json_message);
-//
-//     clients.forEach(client => {
-//         if (client.readyState === WebSocket.OPEN) {
-//             data = {
-//                 text: '',
-//                 score: 0
-//
-//             }
-//             result = {
-//                 results: [json_message.text]
-//             }
-//             request = {
-//                 request: 'set',
-//                 id: randomUUID(),
-//                 result
-//             }
-//             console.log('enviando respuesta')
-//             console.log(request)
-//             client.send(JSON.stringify(transcription), {binary: false});
-//         }
-//     });
-// });
-whisperWebSocket.on('message', (transcription) => {
-    handle_whisper_message(transcription)
-    // const json_message = JSON.parse(transcription);
-    // console.log('Received transcription:', json_message);
-    //
-    // if (json_message.is_final === true) {
-    //     // Extract texts from segments if they exist
-    //     const results = json_message.segments?.map(segment => ({
-    //         text: segment.text,
-    //         score: Math.floor(Math.random() * 101)
-    //     })) || [];
+function sendTranscription(transcription) {
+    const result = {
+        text: transcription, score: Math.floor(Math.random() * 101),
+    }
+    clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            const request = {
+                request: 'set', id: randomUUID(), params: {results: [result]}
+            }
+            client.send(JSON.stringify(request), {binary: false});
+            console.log('response sent');
+        }
+    })
 
-    // clients.forEach(client => {
-    //     if (client.readyState === WebSocket.OPEN) {
-    //         const request = {
-    //             request: 'set',
-    //             id: randomUUID(),
-    //             params: {results}
-    //         };
-    //
-    //         console.log('Sending response:', request);
-    //         client.send(JSON.stringify(request), {binary: false});
-    //         console.log('response sent');
-    //     }
-    // });
-    // }
-});
+}
 
-whisperWebSocket.on('close', (close_status_code, close_msg) => {
-    console.log('[INFO]: Closed connection to Whisper server', close_msg, close_status_code);
-});
+function handle_whisper_status_message(message) {
+    const status = message.status;
+    if (status === "WAIT") {
+        console.info(`[INFO]: Server is full. Estimated wait time ${Math.round(message.message)} minutes.`);
+    } else if (status === "ERROR") {
+        console.error(`Message from Server: ${message.message}`);
+    } else if (status === "WARNING") {
+        console.warn(`Message from Server: ${message.message}`);
+    }
+}
 
-whisperWebSocket.on('error', (err) => {
-    console.error('[ERROR]: Whisper connection failed', err.message);
-});
+previous_segments = null
+last_max_index = -1
+
+function processSegments(segments, is_final) {
+    if (segments !== null && segments.length) {
+        previous_segments = segments
+    }
+    let text = []
+
+    if (is_final && previous_segments) {
+        previous_segments.forEach((segment, index) => {
+
+            if (!text.length || text[text.length - 1] !== segment.text) {
+                if (index > last_max_index) {
+                    text.push(segment.text)
+                    last_max_index = index
+                }
+            }
+
+        })
+
+        const transcription = text.join(' ')
+        console.log('final segments: ', transcription)
+        sendTranscription(transcription)
+    }
+}
+
+function handle_whisper_message(data) {
+    const message = JSON.parse(data);
+
+    if ('status' in message) {
+        handle_whisper_status_message(message);
+    } else if (message.message === "DISCONNECT") {
+        console.info("[INFO]: Server disconnected due to overtime.");
+    } else if (message.message === "SERVER_READY") {
+        console.info(`[INFO]: Server Running`);
+    } else if ("language" in message) {
+        this.language = message.language;
+        const langProb = 'en_US';
+        console.info(`[INFO]: Server detected language ${this.language} with probability ${langProb}`);
+    } else if ("segments" in message) {
+        processSegments(message.segments, message.is_final);
+    }
+}
+
+
+whisperWebSocket.on('message', (transcription) => handle_whisper_message(transcription));
+whisperWebSocket.on('close', (close_status_code, close_msg) => console.log('[INFO]: Closed connection to Whisper server', close_msg, close_status_code));
+whisperWebSocket.on('error', (err) => console.error('[ERROR]: Whisper connection failed', err.message));
 
 // Handle new client connections to your server
 server.on("connection", async (client) => {
@@ -114,24 +116,17 @@ server.on("connection", async (client) => {
     });
 
     await dispatch({
-        codecs: codecs,
-        languages: languages,
-        transport: client,
-        provider: whisperWebSocket
+        codecs: codecs, languages: languages, transport: client, provider: whisperWebSocket
     });
 });
+
+function closeServer(eventName) {
+    server.close(() => {
+        console.log('Server closed on ' + eventName);
+        process.exit(0);
+    });
+}
 
 // Handle server close signals
-process.on("SIGINT", () => {
-    server.close(() => {
-        console.log('Server closed on SIGINT');
-        process.exit(0);
-    });
-});
-
-process.on("SIGTERM", () => {
-    server.close(() => {
-        console.log('Server closed on SIGTERM');
-        process.exit(0);
-    });
-});
+process.on("SIGINT", () => closeServer('SIGINT'));
+process.on("SIGTERM", () => closeServer('SIGTERM'));
